c6b6d026658bc84b15147efdbf431113a9b6d3d5
hyunmin200 <hwanghyunmin0650@gmail.com>
2024-03-21 21:28:25 +0900

    my first server

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleListening = () => {
+	console.log(`server listening on port http://localhost:${PORT} 😭`);
+};
+
+app.listen(PORT, handleListening);
